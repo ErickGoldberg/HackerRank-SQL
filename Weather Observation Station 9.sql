c76a8a0f43ed6e226a2e70a1bb316124569c6a6e
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-04-27 15:29:37 -0300

    Update

--- Weather Observation Station 9.sql
+++ Weather Observation Station 9.sql
@@ -1,5 +1,4 @@
-[16:14, 26/04/2023] Erick Goldberg: Weather Observation Station 9
-[16:14, 26/04/2023] Erick Goldberg: /*
+ /*
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
