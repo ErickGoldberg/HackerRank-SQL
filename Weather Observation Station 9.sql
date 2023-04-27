9344a9bd5c6eda6fc0b7dfd558c87e2a90bb64d1
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-04-27 13:42:59 -0300

    Create Weather Observation Station 9.sql

---
+++ Weather Observation Station 9.sql
@@ -0,0 +1,7 @@
+[16:14, 26/04/2023] Erick Goldberg: Weather Observation Station 9
+[16:14, 26/04/2023] Erick Goldberg: /*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT LIKE '[aeiou]%';
