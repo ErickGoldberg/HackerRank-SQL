ccf824654a62c45f7549e6a0d47b0623854379aa
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-04-29 15:31:14 -0300

    Create Occupations.sql

---
+++ Occupations.sql
@@ -0,0 +1,23 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select
+    Doctor, Professor, Singer, Actor
+from (
+    select
+        NameOrder,
+        max(case Occupation when 'Doctor' then Name end) as Doctor,
+        max(case Occupation when 'Professor' then Name end) as Professor,
+        max(case Occupation when 'Singer' then Name end) as Singer,
+        max(case Occupation when 'Actor' then Name end) as Actor
+    from (
+            select
+                Occupation,
+                Name,
+                row_number() over(partition by Occupation order by Name ASC) as NameOrder
+            from Occupations
+         ) as NameLists
+    group by NameOrder
+    ) as Names
