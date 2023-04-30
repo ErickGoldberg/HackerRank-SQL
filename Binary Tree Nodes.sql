b0383bfe5225accad9fa02b4196498c9a6b870c9
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-04-29 21:21:42 -0300

    Answer advanced

--- Binary Tree Nodes.sql
+++ Binary Tree Nodes.sql
@@ -1,1 +1,12 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
 
+SELECT b.N,
+       (CASE 
+        	 WHEN b.P IS NULL THEN 'Root' 
+             WHEN (SELECT COUNT(*) FROM BST b2 WHERE b2.P = b.N) > 0 THEN 'Inner'
+             ELSE 'Leaf'
+        END)
+FROM bst b ORDER BY N;
