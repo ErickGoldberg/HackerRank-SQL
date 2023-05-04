a36c1960198fe7ab797026037704437e6b9328a6
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-04 13:29:32 -0300

    Answer

--- Weather Observation Station 19.sql
+++ Weather Observation Station 19.sql
@@ -2,3 +2,5 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
+
+select cast(sqrt(power(MIN(Lat_n) - MAX(lat_n),2) + power(MIN(Long_w) - MAX(long_w),2))as decimal(10,4)) from station;
