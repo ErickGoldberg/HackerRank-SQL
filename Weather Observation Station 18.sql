b0ff4f766a8ab943af78ed8f60fe259305937d1b
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-04 13:28:52 -0300

    Answer

--- Weather Observation Station 18.sql
+++ Weather Observation Station 18.sql
@@ -2,3 +2,9 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
+
+SELECT
+    ROUND(ABS(MAX(LAT_N)  - MIN(LAT_N))
+        + ABS(MAX(LONG_W) - MIN(LONG_W)), 4)
+FROM 
+    STATION;
