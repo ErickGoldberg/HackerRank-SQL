0674e10cdd21a65989c9c6abb9d6e8ab4f5a25d7
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-04 13:30:38 -0300

    ANSWER HARD

--- Weather Observation Station 20.sql
+++ Weather Observation Station 20.sql
@@ -3,3 +3,18 @@ Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
 
+SELECT 
+CASE
+        WHEN (SELECT COUNT(*) FROM Station) % 2 = 1 THEN 
+             (SELECT FORMAT(lat_n, 'N4') 
+             FROM Station 
+             ORDER BY lat_n 
+             OFFSET ((SELECT COUNT(*) FROM Station) - 1)/2 ROWS
+             FETCH NEXT 1 ROW ONLY)
+        ELSE (SELECT FORMAT(AVG(t.lat_n), 'N4')
+             FROM (SELECT lat_n 
+                  FROM Station
+                  ORDER BY lat_n 
+                  OFFSET ((SELECT COUNT(*) FROM Station) - 1)/2-1 ROWS
+                  FETCH NEXT 2 ROWS ONLY) t)
+    END AS Median;
