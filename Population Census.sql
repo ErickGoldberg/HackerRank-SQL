a453aaffb741518212b40c74434ed3bd2afa71c8
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-06 22:03:04 -0300

    Update

--- Population Census.sql
+++ Population Census.sql
@@ -2,3 +2,8 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
+
+SELECT sum(city.population)
+FROM city
+INNER JOIN country
+ON city.countrycode = country.code where continent='asia';
