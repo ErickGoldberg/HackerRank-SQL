e737a670c106c8ef07b9e3973c9f5c99d6c3cb99
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-07 23:11:20 -0300

    Answer advanced

--- Contest Leaderboard.sql
+++ Contest Leaderboard.sql
@@ -2,3 +2,12 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
+
+WITH MAXS AS (
+SELECT H.hacker_id, name, challenge_id, max(score) AS maxs FROM Hackers AS H
+    INNER JOIN Submissions AS S ON S.hacker_id = H.hacker_id
+    GROUP BY H.hacker_id, name, challenge_id)
+SELECT hacker_id, name, SUM(maxs) FROM MAXS
+GROUP BY hacker_id, name
+HAVING SUM(maxs) > 0
+ORDER BY 3 DESC, 1;
