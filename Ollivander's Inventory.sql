5602993dfadb316b74afb04247d0918e0f056821
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-07 23:08:00 -0300

    Answer advanced

--- Ollivander's Inventory.sql
+++ Ollivander's Inventory.sql
@@ -2,3 +2,22 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
+
+SELECT
+    W.id,
+    WP.age,
+    W.coins_needed,
+    W.power
+FROM Wands W
+INNER JOIN Wands_Property WP ON W.code = WP.code
+WHERE WP.is_evil != 1
+AND W.coins_needed = (
+    SELECT
+        MIN(W2.coins_needed)
+    FROM Wands W2
+    INNER JOIN Wands_Property WP2 ON W2.code = WP.code
+    WHERE WP2.age = WP.age
+    AND W2.power = W.power)
+ORDER BY
+    W.power DESC,
+    WP.age DESC;
