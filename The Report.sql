dc3d2eb523a450f69cfa9d6937031a99da5434d6
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-07 22:07:58 -0300

    Update The Report.sql

--- The Report.sql
+++ The Report.sql
@@ -2,3 +2,9 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
+
+SELECT 
+    CASE WHEN grade >= 8 THEN name ELSE NULL END, grade, marks
+FROM STUDENTS, GRADES
+WHERE marks BETWEEN MIN_MARK AND MAX_MARK
+ORDER BY GRADE DESC, NAME, MARKS;
